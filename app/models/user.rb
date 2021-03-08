bc0de580cea41ddd3573d84fb30922b9034c8141
takanotakuya <tmw9898@icloud.com>
2021-03-08 14:36:34 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
+  with_options presence: true do
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字（半角）と数字（半角）の両方を含めて設定してください'
+    validates :nickname
+  end
 end
