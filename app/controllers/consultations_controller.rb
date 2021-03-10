65b2803e009a4e5dbb3d9a87c8288f4e6973fc93
takanotakuya <tmw9898@icloud.com>
2021-03-10 14:53:47 +0900

    編集機能追加

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -22,6 +22,21 @@ class ConsultationsController < ApplicationController
     @consultation = Consultation.find(params[:id])
   end
 
+  def edit
+    @consultation = Consultation.find(params[:id])
+    redirect_to consultations_path if @consultation.user != current_user
+  end
+
+  def update
+    @consultation = Consultation.find(params[:id])
+    redirect_to consultations_path if @consultation.user != current_user
+    if @consultation.update(consultation_params)
+      redirect_to consultation_path(@consultation.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def consultation_params
