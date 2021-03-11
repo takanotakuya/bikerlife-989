f20d5516ee9118b3f66d1dfa55e607a45921e183
takanotakuya <tmw9898@icloud.com>
2021-03-11 10:37:37 +0900

    自己紹介カラム追加、表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
+    @self_introduction = user.self_introduction
     @posts = user.posts.includes(:user).order("created_at DESC")
     @consultations = user.consultations.includes(:user).order("created_at DESC")
   end
