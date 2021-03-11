aea6abafe6b16934bf074513f5efa7940050c79d
takanotakuya <tmw9898@icloud.com>
2021-03-11 17:13:24 +0900

    プロフィール編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,30 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @self_introduction = user.self_introduction
-    @posts = user.posts.includes(:user).order("created_at DESC")
-    @consultations = user.consultations.includes(:user).order("created_at DESC")
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @self_introduction = @user.self_introduction
+    @posts = @user.posts.includes(:user).order("created_at DESC")
+    @consultations = @user.consultations.includes(:user).order("created_at DESC")
+    # @post = user.post(params[:id])
+    # @consultation = user.consultation.find(params[:id])
   end
 
   def edit
   end
 
-  def update
+  # def update
+  #   if current_user.update(user_params)
+  #     redirect_to user_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :self_introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root to: 'topbikers#index'
   resources :posts do
     resources :comments, only: :create
