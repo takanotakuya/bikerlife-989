9eef47d9d054f930dc64e32e509c9efc012b920d
takanotakuya <tmw9898@icloud.com>
2021-03-18 10:40:00 +0900

    モデル、テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_005149) do
+ActiveRecord::Schema.define(version: 2021_03_18_013632) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 2021_03_11_005149) do
     t.index ["user_id"], name: "index_consultations_comments_on_user_id"
   end
 
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "post_text"
@@ -91,5 +100,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_005149) do
   add_foreign_key "consultations", "users"
   add_foreign_key "consultations_comments", "consultations"
   add_foreign_key "consultations_comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
