4f39d05bbe01b42b837ccc0750130a8ac222cf79
takanotakuya <tmw9898@icloud.com>
2021-03-18 11:08:42 +0900

    コントローラー記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_post
 
+  def create
+  @like = current_user.likes.new(post_id: @post.id)
+  @like.save
+  @likes = Like.where(post_id: @post.id)
+  end
+
+  def destroy
+  @like = Like.find_by(post_id: @post.id, user_id: current_user.id).destroy
+  @likes = Like.where(post_id: @post.id)
+  end
+
+  private
+
+  def set_post
+  @post = Post.find(params[:post_id])
+  end
 end
