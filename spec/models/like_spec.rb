2623c5b9384c2da96718d353abb25f8eeaa7420a
takanotakuya <tmw9898@icloud.com>
2021-03-20 16:51:07 +0900

    いいね機能モデル単体テスト　実装

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  describe '#create' do
+    context 'いいねできる場合' do
+      it 'ユーザーが紐付いていればいいねできる' do
+        expect(@like).to be_valid
+      end
+    end
+
+    context 'いいねできない場合' do
+      it 'ユーザーが紐付いていなければいいねできない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
 end
+ 
