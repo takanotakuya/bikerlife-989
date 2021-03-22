d3e2928210a9e882277f9375d82774b768f5b5ae
takanotakuya <tmw9898@icloud.com>
2021-03-22 12:45:49 +0900

    コントローラー投稿単体テスト　実装

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 describe PostsController, type: :request do
-
   before do
+    @user = FactoryBot.create(:user)
     @post = FactoryBot.create(:post)
   end
 
@@ -14,13 +14,13 @@ describe PostsController, type: :request do
       get posts_path
       expect(response.body).to include(@post.name)
     end
-    it 'indexアクションにリクエストするとレスポンスに投稿済みの画像URLが存在する' do
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do
       get posts_path
-      expect(response.body).to include(@post.images) 
+      expect(response.body).to include("item-box-img")
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do
       get posts_path
-      expect(response.body).to include('') 
+      expect(response.body).to include('')
     end
   end
 
@@ -33,13 +33,58 @@ describe PostsController, type: :request do
       get post_path(@post)
       expect(response.body).to include(@post.name) 
     end
-    it 'showアクションにリクエストするとレスポンスに投稿済みの画像URLが存在する' do 
+    it 'showアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do 
       get post_path(@post)
-      expect(response.body).to include(@post.images)
+      expect(response.body).to include("item-box-img")
     end
     it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
       get post_path(@post)
       expect(response.body).to include('＜COMMENT 一覧＞')
+    end
+  end 
+
+  describe 'GET #new' do
+    it 'ログインしているユーザーがnewアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'ログインしていないユーザーがnewアクションにリクエストするとログインページに遷移する' do 
+    end
+    it 'newアクションにリクエストするとレスポンスに新規投稿ページが表示される' do
+    end
+  end 
+
+  describe 'POST #create' do
+    it 'createアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'createアクションにリクエストするとDBに投稿済みのテキストが保存されてる' do
+    end
+    it 'createアクションにリクエストするとDBに投稿済みの画像が保存されてる' do 
+    end
+  end 
+
+  describe 'GET #edit' do
+    it 'editアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'editアクションにリクエストするとレスポンスに投稿済みのテキストが表示されている状態で存在する' do
+    end
+    it 'editアクションにリクエストするとレスポンスに投稿済みの画像が表示されている状態で存在する' do 
+    end
+  end 
+
+  describe 'PATCH #update' do
+    it 'updateアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'updateアクションにリクエストするとDBに更新されたテキストが保存される' do
+    end
+    it 'updateアクションにリクエストするとDBに更新された画像が保存される' do 
+    end
+  end 
+
+  describe 'DELETE #destroy' do
+    it 'destroyアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'destroyアクションにリクエストするとレスポンスに投稿済みのテキストが存在しない' do
+    end
+    it 'destroyアクションにリクエストするとレスポンスに投稿済みの画像が存在しない' do 
     end
   end 
 end
